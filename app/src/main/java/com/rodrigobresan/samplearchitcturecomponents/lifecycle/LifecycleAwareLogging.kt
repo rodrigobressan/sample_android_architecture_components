0dff2b0821d79db9d443b9efc60e23cb62b985e5
Rodrigo Bresan <rcbresan@gmail.com>
2018-03-03 15:51:26 -0300

    LifecycleObserver implementation

---
+++ app/src/main/java/com/rodrigobresan/samplearchitcturecomponents/lifecycle/LifecycleAwareLogging.kt
@@ -0,0 +1,22 @@
+package com.rodrigobresan.samplearchitcturecomponents.lifecycle
+
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
+import android.util.Log
+
+class LifecycleAwareLogging : LifecycleObserver {
+
+    private val LOG_TAG = "LifecycleAwareLogging"
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun listeningToCreate() {
+        Log.d(LOG_TAG, "onCreate()")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun listeningToDestroy() {
+        Log.d(LOG_TAG, "onDestroy()")
+    }
+
+}
---
+++ app/src/main/java/com/rodrigobresan/samplearchitcturecomponents/lifecycle/LifecycleSampleActivity.kt
@@ -0,0 +1,16 @@
+package com.rodrigobresan.samplearchitcturecomponents.lifecycle
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.rodrigobresan.samplearchitcturecomponents.R
+
+class LifecycleSampleActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        lifecycle.addObserver(LifecycleAwareLogging())
+    }
+}
